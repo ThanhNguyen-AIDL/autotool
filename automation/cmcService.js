89ab80d0ab6a8350c49422e6e723a2e5589282c4
Steve <stevehuytrannd92@gmail.com>
2025-07-17 14:07:22 +0700

    update chrome path

--- automation/cmcService.js
+++ automation/cmcService.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const fs = require('fs');
 const puppeteer = require('puppeteer-core');
-
+const dotenv = require('dotenv')
 const { doLogin, checkSuspendedAcct, postComment} = require('./authCMC')
 const {canExecute, markExecuted} = require('./cooldownManager')
 const logger = require('../middlewares/logger')
+dotenv.config()
+
 async function doPostArticleCMC({
   name,
   email,
@@ -19,6 +21,7 @@ async function doPostArticleCMC({
   markExecuted("doPostArticleCMC");
 
   const profilePath = path.resolve(process.cwd(), 'profiles', name);
+  const chromePath = process.env.CHROME_PATH || '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
 
   // Ensure profile dir exists
   if (!fs.existsSync(profilePath)) {
@@ -28,7 +31,7 @@ async function doPostArticleCMC({
   let browser = await puppeteer.launch({
     headless: false,
     userDataDir: profilePath,
-    executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome', // adjust for your OS
+    executablePath: chromePath, // adjust for your OS
     args: [
       '--start-maximized',
     ],
