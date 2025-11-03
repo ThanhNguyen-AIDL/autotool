717c946b0e7c70c455b38dc4626b91d23750c931
Steve <stevehuytrannd92@gmail.com>
2025-11-03 13:51:20 +0700

    update db connection pool

--- models/db.js
+++ models/db.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require('sequelize');
-const serverConfig = require('../server.config')
+const serverConfig = require('../server.config');
+const logger = require('../middlewares/logger');
 
 
 const sequelize = new Sequelize(
@@ -17,11 +18,21 @@ const sequelize = new Sequelize(
         rejectUnauthorized: false, // ⚠️ use with caution in production
       },
     },
+    pool: {
+      max: 3,       // maximum number of connections
+      min: 0,        // minimum number of connections
+      acquire: 30000, // max time (ms) to try getting connection before throwing
+      idle: 10000,   // time (ms) before releasing idle connection
+      evict: 1000,   // time (ms) to check and evict idle connections
+    },
+    retry: {
+      max: 5, // number of auto-retries for queries
+    },
   }
 );
 
 sequelize.authenticate()
   .then(() => console.log('Connected to PostgreSQL'))
-  .catch(err => console.error('Database connection error:', err));
+  .catch(err => logger.error('Database connection error:'+ err));
 
 module.exports = sequelize;
